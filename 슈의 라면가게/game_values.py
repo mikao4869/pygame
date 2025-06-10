13e018f9c8fd92d4a281db197ba0cd4c15287c78
박서현 <s24005@gsm.hs.kr>
2025-06-10 21:04:49 +0900

    슈의라면가게

---
+++ 슈의 라면가게/game_values.py
@@ -0,0 +1,16 @@
+# game_values.py
+ingredients = ["라면", "김치", "치즈", "달걀", "떡", "파", "햄", "당근", "고추", "버섯"]
+orders = [
+    ["라면", "김치", "치즈"],
+    ["라면", "달걀", "파"],
+    ["라면", "햄", "버섯"],
+    ["라면", "떡", "고추"]
+]
+CUSTOMER_WAITING = "waiting"
+CUSTOMER_HAPPY = "happy"
+CUSTOMER_ANGRY = "angry"
+customer_images = {
+    CUSTOMER_WAITING: None,  # 실제 경로로 교체 필요
+    CUSTOMER_HAPPY: None,    # 실제 경로로 교체 필요
+    CUSTOMER_ANGRY: None     # 실제 경로로 교체 필요
+}
