ecb6b3b12c4b83fa533bb8f749c11f666fd1f722
박서현 <s24005@gsm.hs.kr>
2025-06-30 09:47:53 +0900

    구구단만들기

---
+++ ExprogramNCS.py
@@ -0,0 +1,8 @@
+import os
+
+for i in range(1, 10):
+    for j in range(1, 10):  
+        print(f"{i} x {j} = {i * j}", end="  ")  
+    print() 
+
+input("Press Enter to continue...")  # Wait for user input before closing the console
